7be5954eeaaf7a1fbcde7869542f45fb401d81d9
Szymon Tymcio <s.tymcio98@gmail.com>
2023-02-17 21:44:00 +0100

    added clock

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 let version = "version-2";
 console.log(`This is ${version}`);
+p = document.getElementById("data");
+let y = new Date().getFullYear();
+let m = new Date().getMonth();
+let d = new Date().getDate();
+p.textContent = `${d}/${m + 1}/${y}`;
