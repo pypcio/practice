32705fa3c71ed7c93db24dfa7276d2b0cc158a28
Szymon Tymcio <s.tymcio98@gmail.com>
2023-02-21 18:31:36 +0100

    update-2

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var _ = require("lodash");
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log("answer", _.without(arr, 3));
 var isSquare = function (n) {
   z = Math.round(Math.sqrt(n));
   return z * z === n ? true : false;
@@ -7,7 +10,7 @@ let a = -1;
 let b = 0;
 let c = 1;
 let d = 39;
-console.log(Math.sqrt(d));
-console.log(isSquare(a));
-console.log(isSquare(b));
-console.log(isSquare(d));
+// console.log(Math.sqrt(d));
+// console.log(isSquare(a));
+// console.log(isSquare(b));
+// console.log(isSquare(d));
